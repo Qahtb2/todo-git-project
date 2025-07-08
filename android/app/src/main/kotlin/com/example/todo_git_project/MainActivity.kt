2406cdc0a9112627491013a392926413be4105a3
qahtan <dnanslman51@gmail.com>
2025-07-08 21:29:19 +0300

    Initial commit: create Flutter project with basic HomeScreen

---
+++ android/app/src/main/kotlin/com/example/todo_git_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_git_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ToDo List')),
+      body: const Center(child: Text('No tasks yet')),
+    );
+  }
+}
