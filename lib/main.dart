567707b668899e7b893bcf85d8ca9a803868bc1f
qahtan <dnanslman51@gmail.com>
2025-07-08 21:39:18 +0300

    feature: implement delete-task functionality

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,77 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> tasks = [];
+  final TextEditingController taskController = TextEditingController();
+
+  void addTask() {
+    final text = taskController.text.trim();
+    if (text.isNotEmpty) {
+      setState(() {
+        tasks.add(text);
+        taskController.clear();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ToDo List')),
-      body: const Center(child: Text('No tasks yet')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: taskController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter new task',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: addTask,
+                  child: const Text('Add'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: tasks.isEmpty
+                  ? const Center(child: Text('No tasks yet'))
+                  : ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(tasks[index]),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        setState(() {
+                          tasks.removeAt(index);
+                        });
+                      },
+                    ),
+                  );
+                },
+              )
+
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
